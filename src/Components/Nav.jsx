9758b6e62be569c6c7a9e7a68fe4b399a8c09243
I .IMRAN <148187844+i-imran09@users.noreply.github.com>
2025-08-04 12:14:33 +0530

    Update Nav.jsx container-sm

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,7 +3,7 @@
 const Nav = (props) => {
   return (
     <nav  className="navbar navbar-expand-lg bg-body-tertiary "data-bs-theme="dark">
-    <div  className="container-fluid">
+    <div  className="container-sm">
       <a  className="navbar-brand"><span className="badge bg-light text-dark fs-4">NewsHunt</span></a>
       <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span  className="navbar-toggler-icon"></span>
