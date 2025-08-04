daf21547487fda9314efef52a000ade93967e13f
I .IMRAN <148187844+i-imran09@users.noreply.github.com>
2025-08-04 13:03:59 +0530

    Update NewsIteam.jsx

--- src/Components/NewsIteam.jsx
+++ src/Components/NewsIteam.jsx
@@ -3,7 +3,7 @@ import image from '../assets/noimage.png'
 
 const NewsIteam = (props) => {
   return (
-       <div className="container-sm bg-dark text-light d-inline-block m-3 me-0 ms-2 px-2 py-2 " style={{ maxWidth: "300px", maxheight:"500px"}}>
+       <div className="container-sm bg-dark text-light d-inline-block m-3 me-0 ms-4 px-2 py-2 " style={{ maxWidth: "300px", maxheight:"500px"}}>
          <div><img src={props.src?props.src:image} style={{height:"200px" ,width:"285px"}} className="img-top" /><br></br></div>
          <div className="">
            <h5 className="">{props.title.slice(0,50)}</h5>
