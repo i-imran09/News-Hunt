9507578f184402644b3a3799453f6eb261f70239
I .IMRAN <148187844+i-imran09@users.noreply.github.com>
2025-08-04 12:37:17 +0530

    Update NewsIteam.jsx

--- src/Components/NewsIteam.jsx
+++ src/Components/NewsIteam.jsx
@@ -3,11 +3,11 @@ import image from '../assets/noimage.png'
 
 const NewsIteam = (props) => {
   return (
-       <div className="container-sm card bg-dark text-light d-inline-block m-5 px-2 py-2 " style={{ maxWidth: "345px" }}>
-         <img src={props.src?props.src:image} style={{height:"200px" ,width:"325px"}} className="card-img-top" />
-         <div className="card-body">
-           <h5 className="card-title">{props.title.slice(0,50)}</h5>
-           <p className="card-text">{props.description?props.description.slice(0,90):"This articale discription was not showing pleas go thorw the link"}</p>
+       <div className="container-sm bg-dark text-light d-inline-block m-5 px-2 py-2 " style={{ maxWidth: "345px", maxheight:"500px"}}>
+         <div><img src={props.src?props.src:image} style={{height:"200px" ,width:"325px"}} className="img-top" /><br></br></div>
+         <div className="">
+           <h5 className="">{props.title.slice(0,50)}</h5>
+           <p className="">{props.description?props.description.slice(0,90):"This articale discription was not showing pleas go thorw the link"}</p>
            <a href={props.url} className="btn btn-primary" target='blank'>
              Read More...
            </a>
