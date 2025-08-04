bd25d7b52ac782c89dbc80638366abc3b938015a
I .IMRAN <148187844+i-imran09@users.noreply.github.com>
2025-08-04 12:49:19 +0530

    Update NewsIteam.jsx width updated

--- src/Components/NewsIteam.jsx
+++ src/Components/NewsIteam.jsx
@@ -3,8 +3,8 @@ import image from '../assets/noimage.png'
 
 const NewsIteam = (props) => {
   return (
-       <div className="container-sm bg-dark text-light d-inline-block m-5 px-2 py-2 " style={{ maxWidth: "345px", maxheight:"500px"}}>
-         <div><img src={props.src?props.src:image} style={{height:"200px" ,width:"325px"}} className="img-top" /><br></br></div>
+       <div className="container-sm bg-dark text-light d-inline-block m-5 px-2 py-2 " style={{ maxWidth: "315px", maxheight:"500px"}}>
+         <div><img src={props.src?props.src:image} style={{height:"200px" ,width:"295px"}} className="img-top" /><br></br></div>
          <div className="">
            <h5 className="">{props.title.slice(0,50)}</h5>
            <p className="">{props.description?props.description.slice(0,90):"This articale discription was not showing pleas go thorw the link"}</p>
