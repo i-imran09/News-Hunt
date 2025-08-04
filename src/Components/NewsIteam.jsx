ab36e92a6c49180f868957300272f277fe5e0c60
I .IMRAN <148187844+i-imran09@users.noreply.github.com>
2025-08-04 12:29:27 +0530

    Update NewsIteam.jsx

--- src/Components/NewsIteam.jsx
+++ src/Components/NewsIteam.jsx
@@ -3,7 +3,7 @@ import image from '../assets/noimage.png'
 
 const NewsIteam = (props) => {
   return (
-       <div className="card bg-dark text-light d-inline-block m-5 px-2 py-2 " style={{ maxWidth: "345px" }}>
+       <div className="container-sm card bg-dark text-light d-inline-block m-5 px-2 py-2 " style={{ maxWidth: "345px" }}>
          <img src={props.src?props.src:image} style={{height:"200px" ,width:"325px"}} className="card-img-top" />
          <div className="card-body">
            <h5 className="card-title">{props.title.slice(0,50)}</h5>
