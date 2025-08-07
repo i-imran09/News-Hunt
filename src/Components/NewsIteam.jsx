89d4051a717b08f9490c25f33723b97b714f17d4
I .IMRAN <iimraniimran4@gmail.com>
2025-08-07 10:20:33 +0530

    updated

--- src/Components/NewsIteam.jsx
+++ src/Components/NewsIteam.jsx
@@ -1,16 +1,17 @@
 import image from '../assets/noimage.png'
+import '../index.css'
 
 
 const NewsIteam = (props) => {
   return (
 
  <div id="Overall">
-        <div class="card" style="max-width: 18rem; max-height: 30rem;">
-            <img src={props.src?props.src:image} class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h5 class="card-title">{props.title.slice(0,50)}</h5>
-              <p class="card-text">{props.description?props.description.slice(0,90):"This articale discription was not showing pleas go thorw the link"}</p>
-              <a href={props.url} class="btn btn-primary">Read more...</a>
+        <div className="card" id="cardallstyle">
+            <img src={props.src?props.src:image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{props.title.slice(0,50)}</h5>
+              <p className="card-text">{props.description?props.description.slice(0,90):"This articale discription was not showing pleas go thorw the link"}</p>
+              <a href={props.url} className="btn btn-primary">Read more...</a>
             </div>
           </div>
 </div>
